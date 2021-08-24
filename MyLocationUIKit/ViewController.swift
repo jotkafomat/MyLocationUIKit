7081f97782b138e88eea68e10d5ad7c6b6454e8c
jotkafomat <k-j@xl.wp.pl>
2021-08-24 15:21:53 +0100

    Initial Commit

---
+++ MyLocationUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocationUIKit
+//
+//  Created by Krzysztof Jankowski on 24/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
