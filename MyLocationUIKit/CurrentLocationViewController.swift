4f6eaa515d907609130861ca2b3723b0731d87aa
jotkafomat <k-j@xl.wp.pl>
2021-08-25 11:42:32 +0100

    display location

--- MyLocationUIKit/CurrentLocationViewController.swift
+++ MyLocationUIKit/CurrentLocationViewController.swift
@@ -11,10 +11,11 @@ import CoreLocation
 class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
+    var location: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateLabels()
     }
     
     //    MARK: - Outlets
@@ -57,7 +58,8 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         didUpdateLocations locations: [CLLocation]
     ) {
         let newLocation = locations.last!
-        print("didUpdateLocations \(newLocation)")
+        location = newLocation
+        updateLabels()
     }
     // MARK: - Helper Methods
     
@@ -76,5 +78,23 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         present(alert, animated: true, completion: nil)
     }
     
+    func updateLabels() {
+        if let location = location {
+            latitudeLabel.text = String(
+                format: "%.8f",
+                location.coordinate.latitude)
+            longitudeLabel.text = String(
+                format: "%.8f",
+                location.coordinate.longitude)
+            tagButton.isHidden = false
+            messageLabel.text = ""
+        } else {
+            latitudeLabel.text = ""
+            longitudeLabel.text = ""
+            addressLabel.text = ""
+            tagButton.isHidden = true
+            messageLabel.text = "Tap 'Get My Location' to Start"
+        }
+    }
 }
 
