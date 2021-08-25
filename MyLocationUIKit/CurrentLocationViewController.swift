410bba548d66af435bd744b9c06435bc774b8541
jotkafomat <k-j@xl.wp.pl>
2021-08-25 11:09:18 +0100

    read location from CoreLocation

--- MyLocationUIKit/CurrentLocationViewController.swift
+++ MyLocationUIKit/CurrentLocationViewController.swift
@@ -6,26 +6,75 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CurrentLocationViewController: UIViewController {
-
+class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    //    MARK: - Outlets
-        @IBOutlet var messageLabel: UILabel!
-        @IBOutlet var latitudeLabel: UILabel!
-        @IBOutlet var longitudeLabel: UILabel!
-        @IBOutlet var addressLabel: UILabel!
-        @IBOutlet var tagButton: UIButton!
-        @IBOutlet var getButton: UIButton!
     
-//    MARK: -Actionc
+    //    MARK: - Outlets
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var tagButton: UIButton!
+    @IBOutlet var getButton: UIButton!
+    
+    //    MARK: -Actionc
     
     @IBAction func getLocation() {
         
+        let authStatus = locationManager.authorizationStatus
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServicesDeniedAlert()
+            return
+        }
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
     }
+    
+    //   MARK: - CLLocationManagerDelegate
+    func locationManager(
+        _ manager: CLLocationManager,
+        didFailWithError error: Error
+    ) {
+        print("didFailWithError \(error.localizedDescription)")
+    }
+    
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        let newLocation = locations.last!
+        print("didUpdateLocations \(newLocation)")
+    }
+    // MARK: - Helper Methods
+    
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(
+            title: "Location Services Disabled",
+            message: "Please enable location services for this app in Settings.",
+            preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
