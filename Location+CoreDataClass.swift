0134beda2d0bcf2f1a22f664b217a05a06a939ec
jotkafomat <k-j@xl.wp.pl>
2021-09-01 10:31:56 +0100

    add Core Data Model

---
+++ Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocationUIKit
+//
+//  Created by Krzysztof Jankowski on 01/09/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
---
+++ Location+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  Location+CoreDataProperties.swift
+//  MyLocationUIKit
+//
+//  Created by Krzysztof Jankowski on 01/09/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Location {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Location> {
+        return NSFetchRequest<Location>(entityName: "Location")
+    }
+
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+    @NSManaged public var date: Date
+    @NSManaged public var locationDescription: String
+    @NSManaged public var category: String
+    @NSManaged public var placemark: CLPlacemark?
+
+}
+
+extension Location : Identifiable {
+
+}
