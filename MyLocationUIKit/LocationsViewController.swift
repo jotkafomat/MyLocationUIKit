10800b4771392777d3fd9298d44a4ffd6ef293d2
jotkafomat <k-j@xl.wp.pl>
2021-09-03 12:41:46 +0100

    add section to locations view

--- MyLocationUIKit/LocationsViewController.swift
+++ MyLocationUIKit/LocationsViewController.swift
@@ -14,33 +14,34 @@ class LocationsViewController: UITableViewController {
     var managedObjectContext: NSManagedObjectContext!
     
     lazy var fetchedResultsController: NSFetchedResultsController<Location> = {
-      let fetchRequest = NSFetchRequest<Location>()
-
-      let entity = Location.entity()
-      fetchRequest.entity = entity
-
-      let sortDescriptor = NSSortDescriptor(
-        key: "date",
-        ascending: true)
-      fetchRequest.sortDescriptors = [sortDescriptor]
-
-      fetchRequest.fetchBatchSize = 20
-
-      let fetchedResultsController = NSFetchedResultsController(
-        fetchRequest: fetchRequest,
-        managedObjectContext: self.managedObjectContext,
-        sectionNameKeyPath: nil,
-        cacheName: "Locations")
-
-      fetchedResultsController.delegate = self
-      return fetchedResultsController
+        let fetchRequest = NSFetchRequest<Location>()
+        
+        let entity = Location.entity()
+        fetchRequest.entity = entity
+        
+        let sort1 = NSSortDescriptor(key: "category", ascending: true)
+        let sort2 = NSSortDescriptor(key: "date", ascending: true)
+        fetchRequest.sortDescriptors = [sort1, sort2]
+        
+        fetchRequest.sortDescriptors = [sort1, sort2]
+        
+        fetchRequest.fetchBatchSize = 20
+        
+        let fetchedResultsController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: self.managedObjectContext,
+            sectionNameKeyPath: "category",
+            cacheName: "Locations")
+        
+        fetchedResultsController.delegate = self
+        return fetchedResultsController
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-          performFetch()
+        performFetch()
         navigationItem.rightBarButtonItem = editButtonItem
- 
+        
     }    
     // MARK: - Table View Delegates
     override func tableView(
@@ -48,7 +49,7 @@ class LocationsViewController: UITableViewController {
         numberOfRowsInSection section: Int
     ) -> Int {
         let sectionInfo = fetchedResultsController.sections![section]
-          return sectionInfo.numberOfObjects
+        return sectionInfo.numberOfObjects
     }
     
     override func tableView(
@@ -58,54 +59,67 @@ class LocationsViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(
             withIdentifier: "LocationCell",
             for: indexPath) as! LocationCell
-
-          let location = fetchedResultsController.object(at: indexPath)
-          cell.configure(for: location)
+        
+        let location = fetchedResultsController.object(at: indexPath)
+        cell.configure(for: location)
         
         return cell
     }
     
     override func tableView(
-      _ tableView: UITableView,
-      commit editingStyle: UITableViewCell.EditingStyle,
-      forRowAt indexPath: IndexPath
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath
     ) {
-      if editingStyle == .delete {
-        let location = fetchedResultsController.object(
-          at: indexPath)
-        managedObjectContext.delete(location)
-        do {
-          try managedObjectContext.save()
-        } catch {
-          print(error)
+        if editingStyle == .delete {
+            let location = fetchedResultsController.object(
+                at: indexPath)
+            managedObjectContext.delete(location)
+            do {
+                try managedObjectContext.save()
+            } catch {
+                print(error)
+            }
         }
-      }
+    }
+    override func numberOfSections(
+      in tableView: UITableView
+    ) -> Int {
+      return fetchedResultsController.sections!.count
+    }
+
+    override func tableView(
+      _ tableView: UITableView,
+      titleForHeaderInSection section: Int
+    ) -> String? {
+      let sectionInfo = fetchedResultsController.sections![section]
+      return sectionInfo.name
     }
     
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      if segue.identifier == "EditLocation" {
-        let controller = segue.destination  as! LocationDetailsTableViewController
-        controller.managedObjectContext = managedObjectContext
-
-        if let indexPath = tableView.indexPath(
-          for: sender as! UITableViewCell) {
-          let location = fetchedResultsController.object(at: indexPath)
-          controller.locationToEdit = location
+        if segue.identifier == "EditLocation" {
+            let controller = segue.destination  as! LocationDetailsTableViewController
+            controller.managedObjectContext = managedObjectContext
+            
+            if let indexPath = tableView.indexPath(
+                for: sender as! UITableViewCell) {
+                let location = fetchedResultsController.object(at: indexPath)
+                controller.locationToEdit = location
+            }
         }
-      }
     }
     
     // MARK: - Helper methods
     func performFetch() {
-      do {
-        try fetchedResultsController.performFetch()
-      } catch {
-        print(error)
-      }
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print(error)
+        }
     }
     
     deinit {
-      fetchedResultsController.delegate = nil
+        fetchedResultsController.delegate = nil
     }
 }
