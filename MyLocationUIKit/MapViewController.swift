04efb2dacecd491cdd84f27b315641d8b88ab53c
jotkafomat <k-j@xl.wp.pl>
2021-09-06 13:56:10 +0100

    add pins to MapView

--- MyLocationUIKit/MapViewController.swift
+++ MyLocationUIKit/MapViewController.swift
@@ -11,23 +11,42 @@ import MapKit
 import CoreData
 
 class MapViewController: UIViewController {
-  @IBOutlet var mapView: MKMapView!
-
-  var managedObjectContext: NSManagedObjectContext!
-
-  // MARK: - Actions
-  @IBAction func showUser() {
-    let region = MKCoordinateRegion(
-      center: mapView.userLocation.coordinate,
-      latitudinalMeters: 1000,
-      longitudinalMeters: 1000)
-    mapView.setRegion(
-      mapView.regionThatFits(region),
-      animated: true)
-  }
-
-  @IBAction func showLocations() {
-  }
+    @IBOutlet var mapView: MKMapView!
+    
+    var locations = [Location]()
+    
+    var managedObjectContext: NSManagedObjectContext!
+    
+    // MARK: - Actions
+    @IBAction func showUser() {
+        let region = MKCoordinateRegion(
+            center: mapView.userLocation.coordinate,
+            latitudinalMeters: 1000,
+            longitudinalMeters: 1000)
+        mapView.setRegion(
+            mapView.regionThatFits(region),
+            animated: true)
+    }
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      updateLocations()
+    }
+    
+    @IBAction func showLocations() {
+    }
+    // MARK: - Helper methods
+    func updateLocations() {
+        mapView.removeAnnotations(locations)
+        
+        let entity = Location.entity()
+        
+        let fetchRequest = NSFetchRequest<Location>()
+        fetchRequest.entity = entity
+        
+        locations = try! managedObjectContext.fetch(fetchRequest)
+        mapView.addAnnotations(locations)
+    }
 }
 
 extension MapViewController: MKMapViewDelegate {
