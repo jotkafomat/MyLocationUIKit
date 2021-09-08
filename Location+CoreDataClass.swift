403da13770b59b79c6bdf21caffa8445195532f5
jotkafomat <k-j@xl.wp.pl>
2021-09-08 08:44:36 +0100

    deleting photo

--- Location+CoreDataClass.swift
+++ Location+CoreDataClass.swift
@@ -13,19 +13,19 @@ import MapKit
 @objc(Location)
 public class Location: NSManagedObject, MKAnnotation {
     public var coordinate: CLLocationCoordinate2D {
-      return CLLocationCoordinate2DMake(latitude, longitude)
+        return CLLocationCoordinate2DMake(latitude, longitude)
     }
-
+    
     public var title: String? {
-      if locationDescription.isEmpty {
-        return "(No Description)"
-      } else {
-        return locationDescription
-      }
+        if locationDescription.isEmpty {
+            return "(No Description)"
+        } else {
+            return locationDescription
+        }
     }
-
+    
     public var subtitle: String? {
-      return category
+        return category
     }
     
     var hasPhoto: Bool {
@@ -33,19 +33,29 @@ public class Location: NSManagedObject, MKAnnotation {
     }
     
     var photoURL: URL {
-      assert(photoID != nil, "No photo ID set")
-      let filename = "Photo-\(photoID!.intValue).jpg"
-      return applicationDocumentsDirectory.appendingPathComponent(filename)
+        assert(photoID != nil, "No photo ID set")
+        let filename = "Photo-\(photoID!.intValue).jpg"
+        return applicationDocumentsDirectory.appendingPathComponent(filename)
     }
     
     var photoImage: UIImage? {
-      return UIImage(contentsOfFile: photoURL.path)
+        return UIImage(contentsOfFile: photoURL.path)
     }
     
     class func nextPhotoID() -> Int {
-      let userDefaults = UserDefaults.standard
-      let currentID = userDefaults.integer(forKey: "PhotoID") + 1
-      userDefaults.set(currentID, forKey: "PhotoID")
-      return currentID
+        let userDefaults = UserDefaults.standard
+        let currentID = userDefaults.integer(forKey: "PhotoID") + 1
+        userDefaults.set(currentID, forKey: "PhotoID")
+        return currentID
+    }
+    
+    func removePhotoFile() {
+        if hasPhoto {
+            do {
+                try FileManager.default.removeItem(at: photoURL)
+            } catch {
+                print("Error removing file: \(error)")
+            }
+        }
     }
 }
